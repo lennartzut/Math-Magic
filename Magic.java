6ad995d3675ad942d36152a7d636c9ec18ebe757
Leo-Zeneo <78357307+Leo-Zeneo@users.noreply.github.com>
2025-01-26 16:23:43 +0100

    Rewrites code with compound assignment operators

--- Magic.java
+++ Magic.java
@@ -3,12 +3,12 @@ public class Magic {
         //Set original number
         int myNumber = 15;
         //Magic math steps
-        int stepOne = myNumber * myNumber;
-        int stepTwo = stepOne + myNumber;
-        int stepThree = stepTwo / myNumber;
-        int stepFour = stepThree + 17;
-        int stepFive = stepFour - myNumber;
-        int stepSix = stepFive / 6;
-        System.out.println(stepSix);
+        int magicNumber = myNumber * myNumber;
+        magicNumber += myNumber;
+        magicNumber /= myNumber;
+        magicNumber += 17;
+        magicNumber -= myNumber;
+        magicNumber /= 6;
+        System.out.println(magicNumber);
 	}
 }
